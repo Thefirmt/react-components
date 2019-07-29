7fbbc5db26199f5a72ac5ce7b8a75ee0e2325259
Alex Essel <thefirmt@gmail.com>
2019-07-29 12:26:54 -0700

    Almost through the excersize but my brain is going to explode.

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // TODO
+var FavPokemon = () => {
+	<div>
+	 <h2> My Favorite Pokemon </h2>
+	 <PokemonList pokes={['Growlithe', 'Arcanine', 'Gastrodon', 'Braviary']}/>
+	</div>
+};
+
+var PokemonList = () => {
+	<ul>
+		<li>{props.todos[0]}</li>
+		<li>{props.todos[1]}</li>
+		<li>{props.todos[2]}</li>
+		<li>{props.todos[3]}</li>
+	</ul>
+};
